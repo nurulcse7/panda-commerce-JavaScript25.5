6ce92a60d54ad8da63c173f91c016a6862512223
Nurul Islam <nurulbdpersonal@gmail.com>
2022-08-19 00:22:07 +0600

    document.js added

---
+++ js/dom-integration.js
@@ -0,0 +1,18 @@
+// ১. নতুন একটা স্ক্রিপ্ট ফাইল খুলবে। সেটাকে index.html এর সাথে কানেক্ট করতে পারো কিনা?
+// Ans.  <script src="js/dom-integration.js"></script> paste in the body tag at last line
+
+// ২. যতগুলা ইলিমেন্টে h3 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার কালার lightblue করেদিতে পারো কিনা। 
+const tagColor = document.getElementsByTagName('h3');
+tagColor.style.color = 'lightblue';
+
+// ৩. ওই ওয়েবসাইট এ backpack আইডি দিয়ে একটা সেকশন আছে। সেই সেকশনের ব্যাকগ্রাউন্ড কালার tomato করে দিতে পারো কিনা। 
+
+const bgColor = document.getElementById('backpack');
+bgColor.style.backgroundColor = 'tomato';
+
+// ৪. card ক্লাস যতগুলা জায়গায় ইউজ করা হয়েছে সবগুলা কার্ড এর বর্ডার রেডিয়াস ৩০px করে দিতে পারো কিনা 
+const cardRadius = document.getElementsByClassName('card');
+cardRadius.style.borderRadius = '30px';
+
+// ৫. কোন একটা ফাংশন লিখো। সেটার ভিতরে কনসোল লগ হবে। এবং সেই ফাংশন টা ক্লিক হ্যান্ডলার হিসেবে কোন একটা বাটনে যোগ করো 
+
